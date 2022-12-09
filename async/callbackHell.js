d35b34250fa614bd29b37cd0f1cfc9c95eaa145d
PepeCharly01 <jc.espinosa2@gmail.com>
2022-12-08 21:44:45 -0600

    callbacks parte 2

---
+++ async/callbackHell.js
@@ -0,0 +1,53 @@
+function hola(nombre, miCallback) {
+    setTimeout(function () {
+        console.log(`Hola, ${nombre}`);
+        miCallback(nombre);
+    }, 2000)
+}
+
+function hablar(callbackHablar) {
+    setTimeout(function() {
+        console.log('Bla bla bla..');
+        callbackHablar();
+    }, 2000)
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log(`Adios ${nombre}`);
+        otroCallback();
+    }, 2000);
+}
+
+function conversacion(nombre, veces, callback) {
+    if (veces > 0) {
+        hablar(function () {
+            conversacion(nombre, --veces, callback);
+    })
+    } else {
+        adios(nombre, callback);
+    }
+}
+
+// --
+console.log('Iniciando un proceso...');
+hola('Carlos', function (nombre) {
+    conversacion(nombre, 3, function() {
+        console.log('Proceso terminado');
+    });
+});
+
+/*
+
+hola('Carlos', function (nombre) {
+    hablar(function() {
+        hablar(function() {
+            hablar(function() {
+                adios(nombre, function() {
+                    console.log('Terminando un proceso..');
+                });
+             });
+        }); 
+    });
+});
+*/
