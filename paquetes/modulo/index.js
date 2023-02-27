bb1ffdc0cf21bfb9b6e4f47e570d7197848a093d
PepeCharly01 <jc.espinosa2@gmail.com>
2023-02-27 10:53:35 -0600

    paquetes npm y exportar modulos

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,7 @@
+// Traer nuestro modulo 
+const modulo = require('./modulo');
+
+// Ejecutar una funcion del modulo
+modulo.saludar();
+console.log(modulo.prop1);
+
---
+++ paquetes/npm/index.js
@@ -0,0 +1,7 @@
+const isOdd = require('is-odd');
+
+const isEven = require('is-even');
+
+
+console.log(isEven(2)+ ' El numero es par');
+console.log(isOdd(3) + ' El numero es inpar');
